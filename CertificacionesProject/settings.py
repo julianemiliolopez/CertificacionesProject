b9f02dc5287f3f3c5075b0c2d14215cee6d8d19b
Ezequiel Ibarra <chimoibarra@gmail.com>
2022-10-05 11:25:20 -0300

    settings y urls

--- CertificacionesProject/settings.py
+++ CertificacionesProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'certificacionesprueba1.apps.certificacionesprueba1Config',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Argentina/Ushuaia'
 
 USE_I18N = True
 
